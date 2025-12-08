e6222045dd315b64c9689ccbd843ebbd3bf6a4ef
StKondrat <kndrt13@icloud.com>
2025-12-08 16:58:22 +0300

    8-12

--- main.cpp
+++ main.cpp
@@ -79,10 +79,48 @@ namespace top
   };
 
   void make_f(IDraw ** b, size_t k);
-  void get_points(IDraw * b, p_t ** ps, size_t & s);
-  frame_t build_frame(const p_t * ps, size_t s);
+  void extend(p_t ** pts, size_t s, p_t p)
+  {
+    p_t * res = new p_t[s + 1];
+    for (size_t i = 0; i < s; ++i)
+    {
+      res[i] = (*pts)[i];
+    }
+    res[s] = p;
+    delete[] *pts;
+    *pts = res;
+  }
+
+  size_t points(const IDraw& d, p_t ** pts, size_t s)
+  {
+
+  }
+
+  size_t get_points(IDraw& b, p_t ** pts, size_t & s)
+  {
+    size_t upd_s = s + 1;
+    p_t * res = new p_t[upd_s];
+    for (size_t i = 0; i < s; ++i)
+    {
+      res[i] = (*pts)[i];
+    }
+    p_t p = b.begin();
+    res[s] = p;
+    delete[] *pts;
+    *pts = res;
+    size_t delta = 1;
+    while (b.next(p) != b.begin())
+    {
+      p = b.next(p);
+      extend(pts, s + delta, p);
+      ++delta;
+    }
+    return delta;
+  }
+
+  frame_t build_frame(const p_t * pts, size_t s);
   char * build_canvas(frame_t f);
-  void paint_canvas(char * cnv, frame_t fr, const p_t * ps, size_t k, char f);
+  void paint_canvas(char * cnv, frame_t fr, const p_t * pts, size_t k, char f);
   void print_canvas(const char * cnv, frame_t fr);
 }
 
@@ -132,4 +170,23 @@ top::p_t top::Dot::begin() const
 top::p_t top::Dot::next(p_t) const
 {
   return begin();
+}
+
+top::frame_t top::build_frame(const p_t * p, size_t s)
+{
+  if (!s) {
+    throw std::logic_error("bad size");
+  }
+  
+  int xmin = p[0].x; int ymin = p[0].y;
+  int xmax = xmax; int ymax = ymin;
+  for (size_t i = 1; i < s; ++i) {
+    xmin = std::min(xmin, p[i].x);
+    ymin = std::min(ymin, p[i].y);
+    xmax = std::max(xmax, p[i].x);
+    ymax = std::max(ymax, p[i].y);
+  }
+  p_t a{xmin, ymin};
+  p_t b{xmax, ymax};
+  return frame_t{a, b};
 }
